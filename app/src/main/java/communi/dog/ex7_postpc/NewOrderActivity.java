61481ce6c8301617eb48a5e9af86a8700c79a83e
lotanaharoni <lotanaharoni@gmail.com>
2021-06-23 21:23:31 +0300

    The base for the newOrderActivity class

--- app/src/main/java/communi/dog/ex7_postpc/NewOrderActivity.java
+++ app/src/main/java/communi/dog/ex7_postpc/NewOrderActivity.java
@@ -1,4 +1,59 @@
 package communi.dog.ex7_postpc;
 
-public class NewOrderActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewOrderActivity extends AppCompatActivity {
+
+    MyLocalDb myLocalDb = null;
+    EditText customerName;
+    EditText pickelsAmount;
+    CheckBox isHumusChecked;
+    CheckBox isTahiniChecked;
+    EditText userComment;
+    Button sendOrderButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order);
+
+        if (myLocalDb == null){
+            myLocalDb = MyAppActivity.getLocalDb();
+        }
+
+        customerName = findViewById(R.id.CustomerName);
+        pickelsAmount = findViewById(R.id.PickelsEditText);
+        isHumusChecked = findViewById(R.id.HummusCheckbox);
+        isTahiniChecked = findViewById(R.id.TahiniCheckbox);
+        userComment = findViewById(R.id.AddCommentEditText);
+        sendOrderButton = findViewById(R.id.sendOrderButton);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("customerName", customerName.getText().toString());
+        outState.putString("pickelsAmount", pickelsAmount.getText().toString());
+        outState.putBoolean("isTahini", isTahiniChecked.isChecked());
+        outState.putBoolean("isHummus", isHumusChecked.isChecked());
+        outState.putString("customerComment", userComment.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        customerName.setText(savedInstanceState.getString("customerName"));
+        pickelsAmount.setText(savedInstanceState.getString("pickelsAmount"));
+        isTahiniChecked.setChecked(savedInstanceState.getBoolean("isTahini"));
+        isHumusChecked.setChecked(savedInstanceState.getBoolean("isHummus"));
+        userComment.setText(savedInstanceState.getString("customerComment"));
+    }
 }
